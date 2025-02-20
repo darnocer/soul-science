81e4fe48fa07827c81bba621a90f50588cdb28eb
Darian Nocera <darnocer@gmail.com>
2025-02-19 20:02:11 -0700

    Add new footer with newsletter, logo, links

--- components/nav/Footer.js
+++ components/nav/Footer.js
@@ -1,31 +1,38 @@
-import Link from '@/components/links/Link'
-import siteMetadata from '@/data/siteMetadata'
+import Logo from '@/components/nav/Logo'
 import footerNavLinks from '@/data/nav/footerNavLinks'
-import SocialIcon from '@/components/icons/social-icons'
+import siteMetadata from '@/data/siteMetadata'
+import Link from '@/components/links/Link'
+import NewsletterForm from '@/components/blocks/NewsletterForm'
 
 export default function Footer() {
   return (
-    <footer className='mt-auto'>
-      <div className='mt-12 flex flex-col items-center font-medium'>
-        <div className='mb-3 flex space-x-4'>
-          {footerNavLinks.map((link, index) => (
-            <span key={link.title} className='flex items-center'>
-              <Link className='text-xxs uppercase text-gray-600 underline dark:text-gray-400' href={link.href}>
+    <footer className='mt-10 border-t border-gray-200 bg-white dark:border-gray-800 dark:bg-black'>
+      <div className='container mx-auto flex max-w-5xl flex-col items-center justify-between px-6 py-8 md:flex-row'>
+        <div className='mb-6 flex max-w-xs flex-col items-center text-center md:mb-0 md:items-start md:text-left'>
+          <Logo type='full' />
+          <p className='mt-3 text-sm font-medium text-gray-600 dark:text-gray-400'>{siteMetadata.description}</p>
+        </div>
+
+        <div className='flex flex-col content-between'>
+          <nav className='mr-6 flex justify-end gap-4 text-sm font-medium text-gray-600 dark:text-gray-400'>
+            {footerNavLinks.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className='font-semibold uppercase underline hover:text-gray-900 dark:hover:text-gray-200'
+              >
                 {link.title}
               </Link>
-              {index < footerNavLinks.length - 1 && <span className='ml-2  text-gray-500 dark:text-gray-400'>|</span>}
-            </span>
-          ))}
+            ))}
+          </nav>
+          <div className='mt-6'>
+            <NewsletterForm />
+          </div>
         </div>
-        <div className='mt-4 mb-2 flex flex-col items-center text-xxs text-gray-500 dark:text-gray-400'>
-          <p className='text-xxs'>
-            Made with 👽 by{' '}
-            <a className='underline' href={siteMetadata.website} target='_blank' rel='noreferrer'>
-              darian.
-            </a>
-          </p>
-          <p>{`© ${new Date().getFullYear()} rootedvision`}</p>
-        </div>
+      </div>
+
+      <div className='py-4 text-center text-xs font-medium text-gray-500 dark:border-gray-800 dark:text-gray-400'>
+        <p>{`© ${new Date().getFullYear()} rootedvision`}</p>
       </div>
     </footer>
   )
