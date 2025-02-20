59081e1c33658e4234c0e309c70ea9d3b2466d3a
Darian Nocera <darnocer@gmail.com>
2025-02-20 10:25:55 -0700

    button styling update

--- components/links/Button.js
+++ components/links/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from '@/components/links/Link'
 
 const Button = ({ text, link, onClick, type = 'primary' }) => {
-  const commonClasses = `exclude-underline ease hover:pointer mb-4 mr-6 inline-flex items-center justify-center gap-2 rounded-md border px-6 py-3 shadow-md transition-all duration-200 hover:no-underline sm:mb-0`
+  const commonClasses = `exclude-underline ease hover:pointer mb-4 inline-flex items-center justify-center gap-2 rounded-md border px-6 py-3 shadow-md transition-all duration-200 hover:no-underline sm:mb-0`
 
   const secondaryClasses = `bg-white border-gray-700 hover:border-gray-500 hover:bg-gray-100 dark:border-gray-300/30 dark:bg-black dark:hover:border-gray-400/40 dark:hover:bg-gray-850`
 
