acba82d49f47acf59dc7a95979df2c48d45f0f04
Darian Nocera <darnocer@gmail.com>
2025-02-19 20:01:28 -0700

    add Hero component

---
+++ components/marketing/Hero.js
@@ -0,0 +1,18 @@
+import Logo from '@/components/nav/Logo'
+import NewsletterForm from '@/components/blocks/NewsletterForm'
+
+export default function Hero({ heading, subtitle, description, disclaimer }) {
+  return (
+    <section className='flex min-h-[50vh] w-full flex-col items-center justify-center px-4  text-center md:min-h-[60vh]'>
+      <Logo />
+      <h1 className='mt-6 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 md:text-5xl lg:text-6xl'>
+        {heading}
+      </h1>
+      <p className='mt-4 max-w-2xl text-xl font-semibold text-gray-500 dark:text-gray-200'>{subtitle}</p>
+      <p className='mt-4 max-w-2xl text-base font-medium text-gray-600 dark:text-gray-300'>{description}</p>
+      <div className='mt-6 w-full max-w-md'>
+        <NewsletterForm />
+      </div>
+    </section>
+  )
+}
