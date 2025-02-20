ca914ddb054524061122a421f54cd46d6bfb1a79
Darian Nocera <darnocer@gmail.com>
2025-02-20 12:49:20 -0700

    BlogSidebar: Styling updates; move to right side

--- components/post/BlogSidebar.js
+++ components/post/BlogSidebar.js
@@ -4,10 +4,10 @@ import LinkArrow from '@/components/links/LinkArrow'
 
 const BlogSidebar = ({ author, tags, next, prev }) => {
   return (
-    <aside className='mt-6 rounded-md p-4 dark:bg-black xl:sticky xl:top-24 xl:col-start-1 xl:row-span-2 xl:max-h-max'>
+    <aside className='mt-6 rounded-md border bg-white p-4 dark:border-gray-700 dark:bg-black xl:sticky xl:top-24 xl:col-start-4 xl:row-span-2 xl:max-h-max'>
       {author && (
         <div className='pb-2 pt-2 xl:border-b xl:border-gray-200 xl:dark:border-gray-700'>
-          <span className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>Author:</span>
+          <span className='text-xs uppercase tracking-wide text-gray-600 dark:text-gray-400'>Author:</span>
           <div className='my-4 space-y-4'>
             <Author author={author} />
           </div>
@@ -16,7 +16,7 @@ const BlogSidebar = ({ author, tags, next, prev }) => {
       <div className='divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:divide-y'>
         {tags && (
           <div className='py-4 xl:py-8'>
-            <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>Tags:</h2>
+            <h2 className='text-xs uppercase tracking-wide text-gray-600 dark:text-gray-400'>Tags:</h2>
             <div className='mt-2 flex flex-wrap gap-y-2'>
               {tags.map((tag) => (
                 <Tag key={tag} text={tag} />
@@ -25,10 +25,10 @@ const BlogSidebar = ({ author, tags, next, prev }) => {
           </div>
         )}
         {(next || prev) && (
-          <div className='flex justify-between py-4 xl:block xl:space-y-8 xl:py-8'>
+          <div className='flex justify-between py-4 xl:block xl:space-y-8 xl:py-8 '>
             {prev && (
-              <div>
-                <span className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>Next Article:</span>
+              <div className=''>
+                <span className='text-xs uppercase tracking-wide text-gray-600 dark:text-gray-400'>Next Article:</span>
                 <div className='mt-2 font-semibold text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
                   <LinkArrow direction='right' href={`/${prev.slug}`} text={prev.title} />
                 </div>
@@ -36,7 +36,7 @@ const BlogSidebar = ({ author, tags, next, prev }) => {
             )}
             {next && (
               <div>
-                <span className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
+                <span className='text-xs uppercase tracking-wide text-gray-600 dark:text-gray-400'>
                   Previous Article:
                 </span>
                 <div className='mt-2 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
