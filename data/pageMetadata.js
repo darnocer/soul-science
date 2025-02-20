d0cda619fc4c766002cb7211a5e1c3533e490c4b
Darian Nocera <darnocer@gmail.com>
2025-02-20 10:26:12 -0700

    cleanup page metadata: remove unnneeded pages

--- data/pageMetadata.js
+++ data/pageMetadata.js
@@ -6,43 +6,10 @@ const pageMetadata = {
     title: '404 - Not Found',
     description: 'Oops!',
   },
-  Spiritual: {
-    title: 'Work - Transformational Wellness',
-    description: 'Spiritual Healing & Transformational Wellness Work',
-  },
-  Management: {
-    title: 'Work - Project Leadership',
-    description: 'Technical Consulting & Project Leadership Work',
-  },
-  Psychedelics: {
-    title: 'Psychedelics',
-    description: 'Psilocybin medicine journeys for spiritual healing.',
-  },
-  Meditation: {
-    title: 'Meditation',
-    description: 'Mindfulness & meditation mentorship',
-  },
-  Writing: {
-    title: 'Writing',
-    description:
-      'Writing on topics including spirituality, psychedelics, health & wellness, and meditation & mindfulness',
-  },
-  Musings: {
-    title: 'Writing - Musings',
-    description: 'Short reflections about the spiritual path.',
-  },
   Posts: {
-    title: 'Writing - All Posts',
+    title: 'All Posts',
     description:
       'All posts on topics including spirituality, psychedelics, health & wellness, and meditation & mindfulness',
-  },
-  Colophon: {
-    title: 'Colophon',
-    description: 'A description of the tools and technologies used to make this website.',
-  },
-  Sitemap: {
-    title: 'Sitemap',
-    description: 'Navigate the full site.',
   },
   Tags: {
     title: 'Topics',
