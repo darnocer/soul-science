f1d7f6e2f1bf793a9e0f2473cad31ca921c8f6f7
Darian Nocera <darnocer@gmail.com>
2025-02-19 19:52:23 -0700

    delete unneeded data, content, pages, and other files from copied repo

--- components/nav/Logo.js
+++ components/nav/Logo.js
@@ -2,23 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import siteMetadata from '@/data/siteMetadata'
 
-const Logo = () => {
+const Logo = ({ size = 'large', type = 'regular' }) => {
+  const sizeClass = size === 'small' ? 'w-[250px]' : ''
+  const logoLight = type === 'full' ? '/static/images/logos/logo-full-light.svg' : '/static/images/logos/logo-light.svg'
+  const logoDark = type === 'full' ? '/static/images/logos/logo-full.svg' : '/static/images/logos/logo.svg'
+
   return (
-    <Link style={{ width: '50px' }} href="/" aria-label={siteMetadata.headerTitle} passHref>
-      <div className="hover:cursor-pointer">
-        <div className="block dark:hidden">
-          <img
-            src="/static/images/logos/logo-light.svg"
-            alt="Logo Light"
-            style={{ width: '100%', height: 'auto', display: 'block' }}
-          />
+    <Link href='/' aria-label={siteMetadata.headerTitle} passHref>
+      <div className={`hover:cursor-pointer ${sizeClass}`}>
+        <div className='block dark:hidden'>
+          <img src={logoLight} alt='Logo Light' className='h-auto w-full' />
         </div>
-        <div className="hidden dark:block">
-          <img
-            src="/static/images/logos/logo.svg"
-            alt="Logo Dark"
-            style={{ width: '100%', height: 'auto', display: 'block' }}
-          />
+        <div className='hidden dark:block'>
+          <img src={logoDark} alt='Logo Dark' className='h-auto w-full' />
         </div>
       </div>
     </Link>
