4eb1ac6ae80b32fedaa8c311f2423fb1bcadb75a
Darian Nocera <darnocer@gmail.com>
2025-02-20 19:06:51 -0700

    Newsletter: Remove SectionContainer

--- components/blocks/NewsletterForm.js
+++ components/blocks/NewsletterForm.js
@@ -48,72 +48,70 @@ const NewsletterForm = ({ title, description, disclaimer }) => {
   }
 
   return (
-    <SectionContainer padding='small'>
-      <div className='relative flex items-center justify-center'>
-        {subscribed && (
-          <div className='absolute left-[75%] top-[50%] w-64 -translate-x-1/2 -translate-y-1/2'>
-            <Lottie animationData={confettiAnimation} autoplay loop={false} />
-          </div>
-        )}
-
-        <div className='justify-content flex flex-col items-start'>
-          {title && <h3 className='pb-1 text-lg font-semibold text-gray-800 dark:text-gray-200'>{title}</h3>}
-          {description && <p className='mb-4 mt-2 text-xs font-medium text-gray-400'>{description}</p>}
-          <div className='mb-10'>
-            <form className='flex max-w-md flex-col flex-wrap sm:flex-nowrap' onSubmit={subscribe}>
-              <div className='flex flex-col md:flex-row'>
-                <label htmlFor='email-input'>
-                  <span className='sr-only'>Email address</span>
-                  <input
-                    autoComplete='email'
-                    className='min-h-[50px] w-72 rounded-md px-4 text-sm font-medium focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black md:min-h-full'
-                    id='email-input'
-                    name='email'
-                    placeholder={subscribed ? content.placeholderSubscribed : content.placeholder}
-                    ref={inputEl}
-                    required
-                    type='email'
-                    disabled={subscribed}
-                  />
-                </label>
-
-                <div className='mt-2 flex rounded-md shadow-sm sm:ml-3 md:mt-0'>
-                  <button
-                    className={`exclude-underline ease hover:pointer mb-0 inline-flex w-full items-center justify-center whitespace-nowrap rounded-md border px-6 py-3 text-sm font-semibold uppercase shadow-md transition-all duration-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black md:py-4 ${
-                      subscribed
-                        ? 'cursor-not-allowed border-gray-600 bg-gray-600/60 text-gray-800 dark:border-gray-600 dark:text-white'
-                        : 'border-primary-600 bg-primary-500 text-white hover:border-primary-500 hover:bg-primary-400 dark:bg-primary-500 dark:text-white dark:hover:border-primary-600 dark:hover:bg-primary-600'
-                    } `}
-                    type='submit'
-                    disabled={subscribed}
-                  >
-                    {subscribed ? 'THANK YOU!' : content.button}
-                  </button>
-                </div>
-              </div>
-              {!subscribed && (
-                <div className='mt-3 flex w-full items-center space-x-2 sm:mt-4'>
-                  <input
-                    type='checkbox'
-                    id='consent-checkbox'
-                    onChange={(e) => setConsent(e.target.checked)}
-                    className='h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-black'
-                  />
-                  <label htmlFor='consent-checkbox' className='text-xs font-medium text-gray-600 dark:text-gray-300'>
-                    {content.consent}
-                  </label>
-                </div>
-              )}
-            </form>
-
-            {error && <div className='w-72 pt-2 text-sm text-accent-500 dark:text-accent-500 sm:w-96'>{message}</div>}
-            {subscribed && <p className='mt-2 text-sm font-medium text-green-600 dark:text-green-400'>{message}</p>}
-          </div>
-
-          {disclaimer && <p className='mt-2 text-xs text-gray-500 dark:text-gray-400'>{disclaimer}</p>}
+    <div className='relative flex items-center justify-center'>
+      {subscribed && (
+        <div className='absolute left-[75%] top-[50%] w-64 -translate-x-1/2 -translate-y-1/2'>
+          <Lottie animationData={confettiAnimation} autoplay loop={false} />
         </div>
+      )}
+
+      <div className='justify-content flex flex-col items-start'>
+        {title && <h3 className='pb-1 text-lg font-semibold text-gray-800 dark:text-gray-200'>{title}</h3>}
+        {description && <p className='mb-4 mt-2 text-xs font-medium text-gray-400'>{description}</p>}
+        <div className='mb-10'>
+          <form className='flex max-w-md flex-col flex-wrap sm:flex-nowrap' onSubmit={subscribe}>
+            <div className='flex flex-col md:flex-row'>
+              <label htmlFor='email-input'>
+                <span className='sr-only'>Email address</span>
+                <input
+                  autoComplete='email'
+                  className='min-h-[50px] w-72 rounded-md px-4 text-sm font-medium focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black md:min-h-full'
+                  id='email-input'
+                  name='email'
+                  placeholder={subscribed ? content.placeholderSubscribed : content.placeholder}
+                  ref={inputEl}
+                  required
+                  type='email'
+                  disabled={subscribed}
+                />
+              </label>
+
+              <div className='mt-2 flex rounded-md shadow-sm sm:ml-3 md:mt-0'>
+                <button
+                  className={`exclude-underline ease hover:pointer mb-0 inline-flex w-full items-center justify-center whitespace-nowrap rounded-md border px-6 py-3 text-sm font-semibold uppercase shadow-md transition-all duration-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black md:py-4 ${
+                    subscribed
+                      ? 'cursor-not-allowed border-gray-600 bg-gray-600/60 text-gray-800 dark:border-gray-600 dark:text-white'
+                      : 'border-primary-600 bg-primary-500 text-white hover:border-primary-500 hover:bg-primary-400 dark:bg-primary-500 dark:text-white dark:hover:border-primary-600 dark:hover:bg-primary-600'
+                  } `}
+                  type='submit'
+                  disabled={subscribed}
+                >
+                  {subscribed ? 'THANK YOU!' : content.button}
+                </button>
+              </div>
+            </div>
+            {!subscribed && (
+              <div className='mt-3 flex w-full items-center space-x-2 sm:mt-4'>
+                <input
+                  type='checkbox'
+                  id='consent-checkbox'
+                  onChange={(e) => setConsent(e.target.checked)}
+                  className='h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-black'
+                />
+                <label htmlFor='consent-checkbox' className='text-xs font-medium text-gray-600 dark:text-gray-300'>
+                  {content.consent}
+                </label>
+              </div>
+            )}
+          </form>
+
+          {error && <div className='w-72 pt-2 text-sm text-accent-500 dark:text-accent-500 sm:w-96'>{message}</div>}
+          {subscribed && <p className='mt-2 text-sm font-medium text-green-600 dark:text-green-400'>{message}</p>}
+        </div>
+
+        {disclaimer && <p className='mt-2 text-xs text-gray-500 dark:text-gray-400'>{disclaimer}</p>}
       </div>
-    </SectionContainer>
+    </div>
   )
 }
 
