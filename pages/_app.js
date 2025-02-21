16988668b40145088334eabb56eda43001729357
Darian Nocera <darnocer@gmail.com>
2025-02-20 19:40:08 -0700

    document error: accidentally removed import statements from app.js, restored

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,25 @@
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import '@/css/previews.css'
+import 'katex/dist/katex.css'
+
+import '@fontsource/inter/variable-full.css'
+
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+
+import { useEffect, useState } from 'react'
+
+import siteMetadata from '@/data/siteMetadata'
+import CustomAnalytics from '@/components/analytics'
+import LayoutWrapper from '@/components/layout/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
+
+import { Analytics } from '@vercel/analytics/react'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
+
 export default function App({ Component, pageProps }) {
   const [mounted, setMounted] = useState(false)
 
@@ -5,7 +27,7 @@ export default function App({ Component, pageProps }) {
     setMounted(true)
   }, [])
 
-  if (!mounted) return null // Prevents rendering in SSR
+  if (!mounted) return null
 
   return (
     <ThemeProvider attribute='class' defaultTheme={siteMetadata.theme}>
@@ -15,6 +37,7 @@ export default function App({ Component, pageProps }) {
       {isDevelopment && isSocket && <ClientReload />}
       <CustomAnalytics />
       <Analytics />
+
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
