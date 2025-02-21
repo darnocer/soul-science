1137a697591a734e2fd360f5f5dec6acb99d1687
Darian Nocera <darnocer@gmail.com>
2025-02-21 08:20:53 -0700

    document error: revert changes; wrap all of index.js in window undefined check

--- pages/index.js
+++ pages/index.js
@@ -21,17 +21,18 @@ export default function Home({ posts, homeContent }) {
   const DEFAULT_LAYOUT = 'ContentLayout'
 
   return (
-    <p>Hello, World</p>
-    // <MetadataWrapper>
-    //   {typeof window !== 'undefined' && (
-    //     <Hero
-    //       heading='Soul Signals'
-    //       subtitle='Microdoses of wisdom for soul searchers and truth seekers.'
-    //       description='Subscribe for short insights on presence, healing, and self-discovery.'
-    //       disclaimer='this is a disclaimer'
-    //     />
-    //   )}
-    //   <CardLayout posts={posts} heading='All Posts' />
-    // </MetadataWrapper>
+    <MetadataWrapper>
+      {typeof window !== 'undefined' && (
+        <>
+          <Hero
+            heading='Soul Signals'
+            subtitle='Microdoses of wisdom for soul searchers and truth seekers.'
+            description='Subscribe for short insights on presence, healing, and self-discovery.'
+            disclaimer='this is a disclaimer'
+          />
+          <CardLayout posts={posts} heading='All Posts' />
+        </>
+      )}
+    </MetadataWrapper>
   )
 }
