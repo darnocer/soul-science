7417c4e45fb10f6cc7be09cace36fc26aac679d0
Darian Nocera <darnocer@gmail.com>
2025-02-22 11:00:34 -0700

    LinkArrow: style updates - chevron is inline with end of text

--- components/links/LinkArrow.js
+++ components/links/LinkArrow.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Link from '@/components/links/Link'
 import ChevronLeft from '@/components/icons/ui/ChevronLeftIcon'
 import ChevronRight from '@/components/icons/ui/ChevronRightIcon'
 
-export default function LinkArrow({ text, direction, href }) {
+export default function LinkArrow({ text, direction = 'left', href }) {
   return (
     <Link href={href}>
-      <div className='my-4 flex justify-start text-base font-semibold leading-6'>
-        <span className='flex items-center text-primary-500 duration-300 ease-in font-sans hover:text-primary-600 hover:underline dark:hover:text-primary-400'>
-          {direction === 'left' && <ChevronLeft className='mr-1' />}
-
-          {text}
-
-          {direction === 'right' && <ChevronRight className='ml-1' />}
-        </span>
+      <div className='my-4 inline-block font-sans text-base font-semibold leading-6 text-primary-500 duration-300 ease-in hover:text-primary-600 hover:underline dark:hover:text-primary-400'>
+        {direction === 'left' && <ChevronLeft className='mr-1 inline h-4 w-4 align-middle' />}
+        <span className='inline'>{text}</span>
+        {direction === 'right' && <ChevronRight className='ml-1 inline h-4 w-4 align-middle' />}
       </div>
     </Link>
   )
