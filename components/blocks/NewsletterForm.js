66198747111be2af1922723c46b22aac8a0272a7
Darian Nocera <darnocer@gmail.com>
2025-02-23 16:47:06 -0700

    NewsletterForm: hide form input/button after subscribe, replace with success message; justify center form

--- components/blocks/NewsletterForm.js
+++ components/blocks/NewsletterForm.js
@@ -66,54 +66,56 @@ const NewsletterForm = ({ title, description, disclaimer = false }) => {
         {title && <h3 className='pb-1 text-xl font-bold text-gray-800 dark:text-gray-200'>{title}</h3>}
         {description && <p className='mb-4 mt-2 text-sm font-medium text-gray-500'>{description}</p>}
 
-        <form className='flex w-full max-w-md flex-col flex-wrap sm:flex-nowrap' onSubmit={subscribe}>
-          <div className='flex flex-col md:flex-row'>
-            <label htmlFor='email-input' className='w-full md:w-72'>
-              <span className='sr-only'>Email address</span>
-              <input
-                autoComplete='email'
-                className='min-h-[50px] w-full rounded-md px-4 text-base font-medium focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black md:min-h-full'
-                id='email-input'
-                name='email'
-                placeholder={subscribed ? content.placeholderSubscribed : content.placeholder}
-                ref={inputEl}
-                required
-                type='email'
-                disabled={subscribed}
-              />
-            </label>
-
-            <div className='mt-2 flex w-full rounded-md shadow-sm sm:ml-3 md:mt-0 md:w-24'>
-              <button
-                className={`exclude-underline ease hover:pointer mb-0 inline-flex w-full items-center justify-center whitespace-nowrap rounded-md border px-6 py-3 text-sm font-semibold uppercase shadow-md transition-all duration-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 disabled:cursor-not-allowed dark:ring-offset-black md:py-4 ${
-                  subscribed
-                    ? 'cursor-not-allowed border-gray-600 bg-gray-600/60 text-gray-800 dark:border-gray-600 dark:text-white'
-                    : 'border-primary-600 bg-primary-500 text-white hover:border-primary-500 hover:bg-primary-400 dark:bg-primary-500 dark:text-white dark:hover:border-primary-600 dark:hover:bg-primary-600'
-                } `}
-                type='submit'
-                disabled={subscribed}
-              >
-                {subscribed ? 'Thanks!' : content.button}
-              </button>
-            </div>
-          </div>
-          {!subscribed && (
-            <div className='mt-3 flex w-full items-start space-x-2 sm:mt-4'>
-              <input
-                type='checkbox'
-                id='consent-checkbox'
-                onChange={(e) => setConsent(e.target.checked)}
-                className='h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-black'
-              />
-              <label
-                htmlFor='consent-checkbox'
-                className='text-left text-xs font-medium leading-tight text-gray-600 dark:text-gray-400'
-              >
-                {content.consent}
+        {!subscribed && (
+          <form className='flex w-full max-w-md flex-col flex-wrap sm:flex-nowrap' onSubmit={subscribe}>
+            <div className='flex flex-col justify-center md:flex-row'>
+              <label htmlFor='email-input' className='w-full md:w-72'>
+                <span className='sr-only'>Email address</span>
+                <input
+                  autoComplete='email'
+                  className='min-h-[50px] w-full rounded-md border-gray-400/40 px-4 text-base font-medium focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black md:min-h-full'
+                  id='email-input'
+                  name='email'
+                  placeholder={content.placeholder}
+                  ref={inputEl}
+                  required
+                  type='email'
+                  disabled={subscribed}
+                />
               </label>
+
+              <div className='mt-2 flex w-full rounded-md shadow-sm sm:ml-3 md:mt-0 md:w-24'>
+                <button
+                  className={`exclude-underline ease hover:pointer mb-0 inline-flex w-full items-center justify-center whitespace-nowrap rounded-md border px-6 py-3 text-sm font-semibold uppercase shadow-md transition-all duration-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 disabled:cursor-not-allowed dark:ring-offset-black md:py-4 ${
+                    subscribed
+                      ? 'cursor-not-allowed border-gray-600 bg-gray-600/60 text-gray-800 dark:border-gray-600 dark:text-white'
+                      : 'border-primary-600 bg-primary-500 text-white hover:border-primary-500 hover:bg-primary-400 dark:bg-primary-500 dark:text-white dark:hover:border-primary-600 dark:hover:bg-primary-600'
+                  } `}
+                  type='submit'
+                  disabled={subscribed}
+                >
+                  {subscribed ? 'Thanks!' : content.button}
+                </button>
+              </div>
             </div>
-          )}
-        </form>
+            {!subscribed && (
+              <div className='mt-3 flex w-full items-start space-x-2 sm:mt-4'>
+                <input
+                  type='checkbox'
+                  id='consent-checkbox'
+                  onChange={(e) => setConsent(e.target.checked)}
+                  className='h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-black'
+                />
+                <label
+                  htmlFor='consent-checkbox'
+                  className='text-left text-xs font-medium leading-tight text-gray-600 dark:text-gray-400'
+                >
+                  {content.consent}
+                </label>
+              </div>
+            )}
+          </form>
+        )}
 
         <Message
           condition={disclaimer}
@@ -122,7 +124,12 @@ const NewsletterForm = ({ title, description, disclaimer = false }) => {
         />
 
         <Message condition={error} text={message} textClasses='text-accent-500 dark:text-accent-500' />
-        <Message condition={subscribed} text={message} textClasses='text-green-600 dark:text-green-400' />
+        <Message
+          condition={subscribed}
+          text={content.placeholderSubscribed}
+          textClasses='min-h-[50px] w-full rounded-md px-4 text-base font-semibold  text-center p-4 border border-gray-400/40 bg-black text-gray-400 dark:text-gray-300'
+        />
+        <Message condition={subscribed} text={message} textClasses='mt-4 text-green-600 dark:text-green-400' />
       </div>
     </div>
   )
