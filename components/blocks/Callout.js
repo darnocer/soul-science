dd612bf12d030de89b1e7efa6ef62cbf40a5f0a9
Darian Nocera <darnocer@gmail.com>
2025-05-14 12:38:02 -0600

    update callout style

--- components/blocks/Callout.js
+++ components/blocks/Callout.js
@@ -1,26 +1,48 @@
-import { Star, Info, Construction, Wrench, Quote } from 'lucide-react'
+'use client'
+
+import { Star, Info, Construction, Wrench, Quote as QuoteIcon } from 'lucide-react'
 import parse from 'html-react-parser'
 
-const Callout = ({ type = 'info', text, title = 'Summary' }) => {
-  const iconSize = 20
+const Callout = ({ type = 'info', title = 'Summary', text, children }) => {
+  const iconSize = 28
+
   const icons = {
     construction: <Construction size={iconSize} />,
-    quote: <Quote size={iconSize} />,
+    quote: <QuoteIcon size={iconSize} />,
     tools: <Wrench size={iconSize} />,
     info: <Info size={iconSize} />,
     default: <Star size={iconSize} />,
   }
-  const icon = icons[type] || icons.default
 
-  const formatText = (inputText) => parse(inputText.replace(/`([^`]+)`/g, "<code className='inline-code'>$1</code>"))
+  const icon = icons[type] || icons.default
+  const isQuote = type === 'quote'
 
   return (
-    <div className='callout not-prose mb-8 mt-8 rounded-md border-2 border-accent-700 bg-white p-4 font-sans text-accent-700 shadow-lg dark:border-accent-400 dark:bg-black dark:text-accent-400'>
-      <div className='flex items-center'>
-        <div className='mr-2'>{icon}</div>
-        <p className='text-md mt-1 font-bold'>{title}</p>
+    <div className='callout not-prose mb-8 mt-8 flex overflow-hidden rounded-md border-2 border-accent-700 bg-white font-sans shadow-lg dark:border-accent-400 dark:bg-black dark:text-gray-200'>
+      <div className='flex w-12 shrink-0 items-start justify-center bg-accent-700 p-2 dark:bg-accent-300'>
+        <div className='mt-2 text-white dark:text-black'>{icon}</div>
       </div>
-      {text && <div className='mt-2 font-semibold text-gray-700 dark:text-gray-300'>{formatText(text)}</div>}
+      <div className='flex flex-col gap-2 p-4'>
+        <p className={`text-lg font-bold ${isQuote ? 'italic' : ''}`}>{title}</p>
+        {text && (
+          <div
+            className={`font-semibold ${
+              isQuote ? 'text-right text-gray-700 dark:text-gray-200' : 'text-gray-700 dark:text-gray-300'
+            }`}
+          >
+            {parse(text)}
+          </div>
+        )}
+        {children && (
+          <div
+            className={`font-semibold ${
+              isQuote ? 'text-right text-gray-800 dark:text-gray-200' : 'text-gray-800 dark:text-gray-200'
+            }`}
+          >
+            {children}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
