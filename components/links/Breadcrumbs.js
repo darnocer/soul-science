3d88d8a98c73bd09035294ed3ed19f26c317e491
Darian Nocera <darnocer@gmail.com>
2025-05-14 12:38:46 -0600

    fix breadcrumb link

--- components/links/Breadcrumbs.js
+++ components/links/Breadcrumbs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import LinkArrow from '@/components/links/LinkArrow'
 
-export default function Breadcrumbs({ href = '/posts', text = 'Back to All Posts', direction = 'left' }) {
+export default function Breadcrumbs({ href = '/blog', text = 'Back to All Posts', direction = 'left' }) {
   return <LinkArrow text={text} direction='left' href={href} />
 }
